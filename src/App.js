b8c51a97b3cd4a0f58534afeb9a5baa0df62bb51
tedLeary <markeleary@gmail.com>
2018-09-26 10:29:33 -0700

    configure sass and css modules

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import styles from './app.module.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <div className={styles.appContainer}>
+        <p>working</p>
+      </div>
+    );
+  }
+}
+
+export default App;
